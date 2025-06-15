d2def32046e3299ef6329910ebf2abe3647e18cf
Ilham Ibrahimli <78649705+IlhamIbrahimli@users.noreply.github.com>
2025-06-15 06:57:04 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,18 +4,67 @@
 #include <SDL3/SDL_main.h>
 #include <string>
 
+#define TILEOFFSET 72+50
+#define TILEXOFFSET 13
+#define TILEWIDTH 40
+#define TILEINTEROFFSET 12
+#define RECTXDISTANCE 139
+#define TEXTWIDTH 84
 
+struct Player {
+  std::string name;
+  int y1;
+  int y2;
+  
+  SDL_Surface* colour;
+
+  
+};
+
+enum State {
+  DICE,
+  P1TURN,
+  P2TURN,
+  MOVING
+};
+
+/*
+ Dices function :
+ Always draw current dice pos
+ If spinning increase dice pos and render number based on floored value
+ Increase for a random amount of time
+*/
+
+/* 
+ * Cases that can happen:
+ * If dice = 1,2,3 select a piece and move it forward that many places
+ * Can't happen if another same colour piece is already at that space unless it is 1 or 11 or 5
+ * If another player's piece is on the square you moved to, send that piece back to start Unless this happens on tile index 5 or tile index 11 or 1 (obviously)
+ * If dice = 4 select a piece to move back 1 space
+ * Again same as before. Cant happen if piec already there, unless 0 or 10 or 4
+ * And same about previous piece.
+ */
 
 const int winWidth = 480;
 const int winHeight = 640;
-SDL_Surface* surfaces[20];
+const int p1X1 = RECTXDISTANCE + TILEXOFFSET;
+const int p1X2 = p1X1 + TILEWIDTH + TILEINTEROFFSET;
+const int p2X1 = p1X2 + TILEWIDTH+  TILEXOFFSET + TEXTWIDTH + TILEXOFFSET;
+const int p2X2 = p2X1 + TILEWIDTH + TILEINTEROFFSET;
 SDL_Window* gameWindow = nullptr;
 SDL_Surface* winSurface = nullptr;
 SDL_Surface* bgSurface = nullptr;
 SDL_Surface* redSurface = nullptr;
 SDL_Surface* blackSurface = nullptr;
-std::string p1Name = "Player 1";
-std::string p2Name = "Player 2";
+struct Player p1 = {"Player 1", 1 , 1 , redSurface};
+struct Player p2 = {"Player 2", 1 , 1, blackSurface};
+SDL_Rect p1piece1 = {p1X1, 640-(TILEOFFSET*p1.y1)+6, 40,40};
+SDL_Rect p1piece2 =  {p1X2, 640-(TILEOFFSET*p1.y2)+6, 40,40};
+SDL_Rect p2piece1 =  {p2X1, 640-(TILEOFFSET*p2.y1)+6, 40,40};
+SDL_Rect p2piece2 =  {p2X2, 640-(TILEOFFSET*p2.y2)+6, 40,40};
+float dicePos = 0;
+
+
 
 bool init() {
   bool success = true;
@@ -33,6 +82,7 @@ bool init() {
     } else {
       winSurface = SDL_GetWindowSurface(gameWindow);
     }
+    SDL_SetRenderVSync( winSurface, 1 );
   }
 
 
@@ -54,22 +104,12 @@ bool loadImage(std::string imgPath, SDL_Surface** surface) {
     std::cout << "Cant load img with path:" << imagePath << std::endl;
 
   }
-  for (int j = 0; j < 20; j++) {
-    if (surfaces[j] == nullptr) {
-      surfaces[j] = *surface;
-    }
-  }
+  
   return success;
 }
 
 
 bool destroyImage(SDL_Surface** surface) {
-  for (int i = 0; i <20; i++) {
-    if (*surface == surfaces[i] ) {
-      surfaces[i] == nullptr;
-    }
-  }
-
   SDL_DestroySurface(*surface);
   surface = nullptr;
   return true;
@@ -79,13 +119,12 @@ bool destroyImage(SDL_Surface** surface) {
 
 
 bool close() {
-  for (int i = 0; i < 20; i++) {
-    if (surfaces[i] != nullptr) {
-      destroyImage(&surfaces[i]);
-    }
-  }
-  std::cout << surfaces;
+  destroyImage(&winSurface);
+  destroyImage(&bgSurface);
+  destroyImage(&redSurface);
+  destroyImage(&blackSurface);
   SDL_DestroyWindow(gameWindow);
+
   gameWindow = nullptr;
   winSurface = nullptr;
   return true;
@@ -94,24 +133,33 @@ bool close() {
 int main() {
   init();
   loadImage("bg.bmp", &bgSurface);
+  loadImage("redChar.bmp", &redSurface);
+  loadImage("blackChar.bmp", &blackSurface);
+
+  p1.colour = redSurface;
+  p2.colour = blackSurface;
   bool quit = false;
   bool skib = false;
   SDL_Event e;
   //std::cout << surfaces[0];
   while (quit == false) {
+    SDL_FillSurfaceRect(winSurface, nullptr, SDL_MapSurfaceRGB(winSurface, 255, 255, 255));
+    SDL_BlitSurface(bgSurface, nullptr, winSurface, nullptr);
+    SDL_BlitSurface(p1.colour, nullptr, winSurface, &p1piece1 );
+    SDL_BlitSurface(p1.colour, nullptr, winSurface, &p1piece2 );
+    SDL_BlitSurface(p2.colour, nullptr, winSurface, &p2piece1 );
+    SDL_BlitSurface(p2.colour, nullptr, winSurface, &p2piece2 );
+
     while (SDL_PollEvent(&e)) {
       if (e.type == SDL_EVENT_QUIT) {
         quit = true;
       }
       else if (e.type == SDL_EVENT_KEY_DOWN) {
-        skib = true;
+        p1.y1 = p1.y1 < 11 ? p1.y1 + 1 : p1.y1;
+        p1piece1.y = 640-(TILEOFFSET*p1.y1)+6;
       }
     }
 
-    SDL_FillSurfaceRect(winSurface, nullptr, SDL_MapSurfaceRGB(winSurface, 255, 255, 255));
-    if (skib) {
-      SDL_BlitSurface(bgSurface, nullptr, winSurface, nullptr);
-    }
     SDL_UpdateWindowSurface(gameWindow);
   }
   close();
